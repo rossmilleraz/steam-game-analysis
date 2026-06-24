6b83f44346b2717b5b9f8efb8645644933713198
Ross Miller <rossmilleraz@gmail.com>
2026-06-23 22:13:45 -0700

    Initial main.py file

---
+++ main.py
@@ -0,0 +1,18 @@
+# This file runs the whole project from start to finish.
+# One main file so I do not have to run every script manually
+
+def main():
+    print("Cleaning Steam data...")
+    clean_steam_data()
+
+    print("Creating summary tables...")
+    create_summary_tables()
+
+    print("Creating charts...")
+    create_charts()
+
+    print("Project finished.")
+
+
+if __name__ == "__main__":
+    main()
